2241f840ceaaa8964a4aad9a2b843835e85be80f
Yanut Chaiprasert <119662174+toricago@users.noreply.github.com>
2023-03-11 01:30:58 +0900

    logo added

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,6 @@
-import download from './download.png';
-import logo from './logo.svg';
-import preview from './preview.png';
+import download from "./download.png"
+import logo from "./logo.svg"
+import pixelia from "./pixelia.png"
+import preview from "./preview.png"
 
-export {
-  download,
-  logo,
-  preview,
-};
+export { download, logo, preview, pixelia }
